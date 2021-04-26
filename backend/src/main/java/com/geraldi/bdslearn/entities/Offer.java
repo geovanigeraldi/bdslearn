08a3eb8f5fcc8c2b713d6b1c6a381b2ac9effa58
Geovani Geraldi <geovani.geraldi@gmail.com>
2021-04-26 10:04:01 -0300

    resource

--- backend/src/main/java/com/geraldi/bdslearn/entities/Offer.java
+++ backend/src/main/java/com/geraldi/bdslearn/entities/Offer.java
@@ -2,6 +2,8 @@ package com.geraldi.bdslearn.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -21,17 +24,23 @@ public class Offer implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String edition;
-	
+
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant startMoment;
 
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant endMoment;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "course_id")
 	private Course course;
-	
+
+	@OneToMany(mappedBy = "offer")
+	private List<Resource> resources = new ArrayList<>();
+
+	// @OneToMany(mappedBy = "offer")
+	// private List<Topic> topics = new ArrayList<>();
+
 	public Offer() {
 	}
 
@@ -82,7 +91,11 @@ public class Offer implements Serializable {
 
 	public void setCourse(Course course) {
 		this.course = course;
-	}	
+	}
+
+	public List<Resource> getResources() {
+		return resources;
+	}
 
 	@Override
 	public int hashCode() {
