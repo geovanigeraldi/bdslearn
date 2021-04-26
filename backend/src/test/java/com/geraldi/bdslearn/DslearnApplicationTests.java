ba62fb3b3fd6f14a3d8d4df8d39f2d1dc0dda7a7
Geovani Geraldi <geovani.geraldi@gmail.com>
2021-04-26 08:50:42 -0300

    project created

---
+++ backend/src/test/java/com/geraldi/bdslearn/DslearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geraldi.bdslearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
