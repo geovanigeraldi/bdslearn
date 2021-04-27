c3ba491e902d376a4e17d3909bfd432ab6247f99
Geovani Geraldi <geovani.geraldi@gmail.com>
2021-04-27 10:29:39 -0300

    fix

--- backend/src/main/java/com/geraldi/bdslearn/entities/Offer.java
+++ backend/src/main/java/com/geraldi/bdslearn/entities/Offer.java
@@ -38,8 +38,8 @@ public class Offer implements Serializable {
 	@OneToMany(mappedBy = "offer")
 	private List<Resource> resources = new ArrayList<>();
 
-	// @OneToMany(mappedBy = "offer")
-	// private List<Topic> topics = new ArrayList<>();
+	@OneToMany(mappedBy = "offer")
+	private List<Topic> topics = new ArrayList<>();
 
 	public Offer() {
 	}
@@ -97,6 +97,10 @@ public class Offer implements Serializable {
 		return resources;
 	}
 
+	public List<Topic> getTopics() {
+		return topics;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
